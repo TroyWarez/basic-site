50cae5e60098bb73e2677db6aa08941ba1b3a48d
Troy <TroyWarez@troysdomain.com>
2024-03-30 16:53:35 -0700

    basic data

--- src/components/OrderForm.tsx
+++ src/components/OrderForm.tsx
@@ -4,14 +4,22 @@ const OrderForm = () => {
     return (
         <>
             <form action="" method="get" className="form-example">
-                <input type="text" name="name" id="firstname" placeholder="First Name" required />
-                <input type="text" name="name" id="lastname" placeholder="Last Name" required />
-                <input type="email" name="email" id="email" required placeholder="Email"/>
-                <input type="text" name="name" id="address" required placeholder="Address"/>
-                <input type="text" name="name" id="country" required placeholder="Country"/>
-                <input type="text" name="name" id="ZIP Code" required placeholder="ZIP Code"/>
-                <input type="text" name="name" id="City" required placeholder="City"/>
-                <input type="text" name="name" id="State" required placeholder="State"/>
+                <input type="text" name="name" id="firstname" placeholder="First Name" maxLength={50} required />
+
+                <input type="text" name="name" id="lastname" placeholder="Last Name" maxLength={50} required />
+
+                <input type="email" name="email" id="email" required placeholder="Email" maxLength={62}/>
+
+                <input type="text" name="address" id="address" required placeholder="Address" maxLength={95}/>
+
+                <input type="text" name="City" id="City" required placeholder="City" maxLength={35}/>
+
+                <input type="text" name="ZIP/Postal Code" id="ZIP/Postal Code" required placeholder="ZIP/Postal Code" maxLength={10}/>
+
+                <input type="text" name="State/Province/Territory" id="State/Province/Territory" required placeholder="State/Province/Territory" maxLength={35}/>
+
+                <input type="text" name="country" id="country" required placeholder="Country" maxLength={95}/>
+
                 <input type="submit" id="submit" value="Submit Order" />
             </form>
         </>
