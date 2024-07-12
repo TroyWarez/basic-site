c16814f117c9dfb02e0fa0e76ec0a5ab51b30d95
TroyWarez <TroyWarez@troysdomain.com>
2024-07-12 09:41:18 -0700

    css and footer

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -4,8 +4,10 @@ const NotFoundPage = () => {
   return (
     <div>
       <title>Page not found</title>
-      <h1>404 Page not found.</h1>
+      <h1>page not found</h1>
+      <p>the page you requested does not exist</p>
       <Link to="/">Go back to home page</Link>
+      <footer>© Copyright {new Date().getFullYear()} all rights reserved</footer>
     </div>
   );
 };
