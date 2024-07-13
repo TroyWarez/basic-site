b89e4b3142dcac59f3839d8bc63e0a01706d2688
TroyWarez <TroyWarez@troysdomain.com>
2024-07-13 16:51:07 -0700

    basic form

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -7,6 +7,9 @@ const PaymentForm = () => {
         <form className={classes.form}>
         <h3>Payment</h3>
         <p>All transactions are secure and encrypted.</p>
+        <input type="text" name="Card number" id="cardnumber" required placeholder="Card Number" maxLength={16}/>
+
+        <input type="text" name="Expiration date (MM / YY)" id="expirationdate" required placeholder="Expiration date (MM / YY)" maxLength={5}/>
         </form>
     </div>
     </>
