087aaf991fb587327a6144daeed11c6f426798ee
TroyWarez <TroyWarez@troysdomain.com>
2024-07-25 12:17:38 -0700

    rewrite

--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -375,6 +375,18 @@ const OrderForm = () => {
                 <option value="NT">Northwest Territories</option>
                 <option value="NU">Nunavut</option>
                 </select>
+
+                  <div id={classes.paymentBlock}>
+                  <h3 id={classes.paymentheading}>Payment</h3>
+                  <p id={classes.paymentSubtext} >All transactions are secure and encrypted.</p>
+                  <Input id={classes.cardnumber} type="text" name="Card number" required={true} placeholder="Card Number" maxLength={16}/>
+                  <div id={classes.securitycodeBlock}>
+                    <Input type="text" name="Expiration date (MM / YY)" id={classes.expireDate} required placeholder="Expiration date (MM / YY)" maxLength={5}/>
+                    <Input type="text" name="Security Code" id={classes.securitycode} required={true} placeholder="Security Code" maxLength={4}/>
+                  </div>
+
+                  <Input type="submit" name="submit" id={classes.submit} required={true} placeholder="Submit Order" maxLength={95}/>
+                </div>
             </form>
         </div>
         </>
--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,13 +1,11 @@
 import OrderForm from "../components/OrderForm/OrderForm";
 import Homebutton from "../components/Homebutton/Homebutton";
-import PaymentForm from "../components/PaymentForm";
 const CheckoutPage = () => {
   return (
     <div>
     <title>Store Checkout</title>
     <Homebutton/>
     <OrderForm/>
-    <PaymentForm/>
     <footer>© Copyright {new Date().getFullYear()} all rights reserved</footer>
     </div>
   );
