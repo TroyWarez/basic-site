9acf25a4675a963dbd991098107b9c52055829ac
Troy <TroyWarez@troysdomain.com>
2024-08-31 19:01:14 -0700

    telephone number formating

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -15,9 +15,33 @@ function Input(props : React.InputHTMLAttributes<HTMLInputElement>) : JSX.Elemen
                   }
                 case "tel":
                     {
-                      event.target.value = event.target.value.replace(/[^\d+\-().\s#*]/g, "");
-                      if(event.target.value !== '') {
+                      event.target.value = event.target.value.replace(/[^\d!@$%^`~+\-().\s#*]/g, "");
+                      
+                      if(event.target.value !== '' && event.target.value.length < 14) {
+                        if(event.target.value.length > 2 && (event.target.value.length < 11))
+                        {
+                          if( event.target.value.length > 2 && event.target.value[0] !== '(' && event.target.value[3] !== ')' ||
+                            event.target.value[0] !== '(' && event.target.value[4] !== ')' ) 
+                          {
+                            event.target.value = '(' + event.target.value.slice(0, 3) + ')' + event.target.value.slice(3, event.target.value.length);
+                          }
+                          if ( event.target.value.length > 5 && event.target.value[0] === '(' && event.target.value[4] === ')' && event.target.value[5] !== ' ')
+                          {
+                            event.target.value = event.target.value.slice(0, 5) + ' ' + event.target.value.slice(5, event.target.value.length);
+                          }
+                          if( event.target.value.length > 9 && event.target.value[0] === '(' && event.target.value[4] === ')' && event.target.value[5] === ' ' && event.target.value[9] !== '-')
+                          {
+                            event.target.value = event.target.value.slice(0, 9) + '-' + event.target.value.slice(9, event.target.value.length);
+                          }
+                        }
 
+                      }
+                      else if (event.target.value.length > 11)
+                      {
+                        if(isNaN(Number(event.target.value)))
+                          {
+                            event.target.value = event.target.value.replace(/\D/g, "");
+                          }
                       }
                       break;
                   }
