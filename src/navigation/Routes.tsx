3dc8413f77b88591f96fc90de62441e5a1267bed
Troy <TroyWarez@troysdomain.com>
2024-09-12 11:31:26 -0700

    revert 2

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import CheckoutPage from "../pages/CheckoutPage/CheckoutPage"
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage"
 
-const AppRoutes = ()  : JSX.Element => (
+const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<CheckoutPage />} />
     <Route path="*" element={<NotFoundPage />} />
