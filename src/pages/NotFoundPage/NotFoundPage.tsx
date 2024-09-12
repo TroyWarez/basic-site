581928b18eb93902a7a9c4da8e234e9d71648fc7
Troy <TroyWarez@troysdomain.com>
2024-09-12 11:25:19 -0700

    revert

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const NotFoundPage = () : JSX.Element => {
+const NotFoundPage = () => {
   return (
     <div>
       <title>404 Not found - Store</title>
