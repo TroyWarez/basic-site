cbd397021082b9e16902bd47bf311a86794e0a48
Troy <TroyWarez@troysdomain.com>
2024-09-26 03:23:45 -0700

    css

--- src/components/CheckoutCart/CheckoutCart.tsx
+++ src/components/CheckoutCart/CheckoutCart.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import classes from "../CheckoutCart/CheckoutCart.module.css"
+interface CartProps {
+  className?: string;
+}
 const CheckoutCart = () => {
   return (
+    <>
     <div className={classes.CheckoutCart}>
         <img src="Toy Car.jpg" alt='ProductImage'/>
-        <p>{'$29.99 USD'}</p>
     </div>
+            <p className={classes.p}>{'$29.99 USD'}</p>
+            </>
   )
 }
 export default CheckoutCart
