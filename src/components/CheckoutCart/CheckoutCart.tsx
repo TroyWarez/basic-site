983bdc83b19e6655cbb90e6e7faf0cb570d91a05
Troy <TroyWarez@troysdomain.com>
2024-09-30 23:48:39 -0700

    better cart

--- src/components/CheckoutCart/CheckoutCart.tsx
+++ src/components/CheckoutCart/CheckoutCart.tsx
@@ -5,7 +5,7 @@ interface CartProps {
   className?: string;
 }
 const CheckoutCart = ({ cartItems, className}: CartProps) => {
-  let subtotal = 0.00;
+  const subTotal = { displayCurrencyValue : 0.00, displayCurrencyValueType : ''};
   return (
     <>
     <div>
@@ -14,9 +14,14 @@ const CheckoutCart = ({ cartItems, className}: CartProps) => {
               <p className={classes.quantityNumber}>{cartItem.quantityNumber}</p>
               <img src={cartItem.productImagePath} alt='ProductImage'/>
               <p className={classes.p}>{cartItem.displayItemName}</p>
-              <p className={classes.p}>{`Subtotal * ${cartItems.length} Items $ ${subtotal = subtotal + cartItem.displayCurrencyValue} ${cartItem.displayCurrencyValueType}`}</p>
+              <p className={classes.p}>{`Price $ ${cartItem.displayCurrencyValue} ${cartItem.displayCurrencyValueType}` }</p>
+              <div hidden={true}>{subTotal.displayCurrencyValue =+ cartItem.displayCurrencyValue}
+                                  {subTotal.displayCurrencyValueType = cartItem.displayCurrencyValueType}
+              </div>
           </div>
-    ))}
+    ))
+    }
+              <p className={classes.p}>{`Subtotal * ${cartItems.length} Items $ ${subTotal.displayCurrencyValue} ${subTotal.displayCurrencyValueType}`}</p>
     </div>
     </>
 
--- src/models/CartItem.tsx
+++ src/models/CartItem.tsx
@@ -3,7 +3,7 @@ export default interface CartItem {
     sku?: number | string;
     displayItemName?: string;
     displayCurrencyValue: number;
-    displayCurrencyValueType?: string;
+    displayCurrencyValueType: string;
     productImagePath?: string;
     quantityNumber?: number;
 }
