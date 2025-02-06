4041817311bbc7913100b8ec279a519250ec26ab
Troy <TroyWarez@troysdomain.com>
2025-02-06 15:25:37 -0700

    added state to the component

--- src/components/CouponForm/CouponForm.tsx
+++ src/components/CouponForm/CouponForm.tsx
@@ -1,33 +1,32 @@
 import FormInput from "../FormInput/FormInput"
 import classes from '../CouponForm/CouponForm.module.css'
 import ids from '../CouponForm/CouponForm.module.css'
-
-const OnClickHandler = (event: React.PointerEvent<HTMLInputElement>) => {
-  switch (event.type) {
-    case "click":
-      {
-        const CouponCodeInputElement = document.getElementById(ids.CouponCodeInput) as HTMLInputElement;
-        if (CouponCodeInputElement) {
-          console.log(CouponCodeInputElement.value);
-        }
-      }
-  }
-}
-const OnInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-  switch (event.type) {
-    case "input": {
-      event.target.value = event.target.value.toUpperCase();
-      console.log(event.target.value);
-    }
-  }
-}
+import { useState } from "react"
 const CouponForm = () : JSX.Element => {
+  const [couponText, setCouponText] = useState("");
   return (
     <>
     <form id={ids.CouponForm}>
-    <FormInput className={classes.CouponInput} type="text" name="coupon" required placeholder="Coupon code" maxLength={95} id={ids.CouponCodeInput} onInput={OnInputHandler}/>
+    <FormInput className={classes.CouponInput} type="text" name="coupon" required placeholder="Coupon code" maxLength={10} id={ids.CouponCodeInput} onInput={ 
+      (event: React.ChangeEvent<HTMLInputElement>) => {
+      switch (event.type) {
+        case "input": {
+        event.target.value = event.target.value.toUpperCase();
+        setCouponText(event.target.value);
+      }
+    }
+  } }/>
     </form>
-    <FormInput className={classes.CouponInput} form={ids.CouponForm} type="button" name="couponButton" value="Apply" required onClick={OnClickHandler}/>
+    <FormInput className={classes.CouponInput} form={ids.CouponForm} type="button" name="couponButton" value="Apply" required onClick={(event: React.PointerEvent<HTMLInputElement>) => {
+      switch (event.type) {
+        case "click":
+          {
+            if (couponText != "") {
+              console.log(couponText);
+            }
+          }
+      }
+}}/>
     </>
   )
 }
