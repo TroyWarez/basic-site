0dceca4c8512cacb7758181762e4494be9b708fd
Troy <TroyWarez@troysdomain.com>
2025-02-06 21:09:04 -0700

    removed code

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -4,8 +4,6 @@ interface FormInputProps {
   className?: string | undefined;
   onInput?: (event: React.ChangeEvent<HTMLInputElement>) => void | undefined;
   onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void | undefined;
-  onClick?: (event: React.PointerEvent<HTMLInputElement>) => void | undefined;
-  onKeyUp?: (event: React.KeyboardEvent<HTMLInputElement>) => void | undefined;
   title?: string | undefined;
   placeholder?: string | undefined;
   inputMode?: "none" | "text" | "tel" | "url" | "email" | "numeric" | "decimal" | "search" | undefined;
@@ -24,8 +22,6 @@ const FormInput = (
   {className,
   onInput, 
   onBlur, 
-  onClick,
-  onKeyUp,
   title, 
   placeholder, 
   inputMode, 
@@ -43,9 +39,7 @@ const FormInput = (
     <input
     className={(!className) ? classes.input : `${className} ${classes.input}`}
     onInput={onInput}
-    onKeyUp={onKeyUp}
     onBlur={onBlur}
-    onClick={onClick}
     title={title}
     placeholder={placeholder}
     inputMode={inputMode}
