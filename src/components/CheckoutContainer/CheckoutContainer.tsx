f387bbd59241934bac1df5b94ffd64ddde630e66
Troy <TroyWarez@troysdomain.com>
2025-02-26 15:24:14 -0700

    removed useState and took simpler approach

--- src/components/CheckoutContainer/CheckoutContainer.tsx
+++ src/components/CheckoutContainer/CheckoutContainer.tsx
@@ -2,13 +2,9 @@ import classes from './CheckoutContainer.module.css'
 import CheckoutCart from '../CheckoutCart/CheckoutCart'
 import CartItem from "../../models/CartItem.tsx";
 import OrderForm from '../OrderForm/OrderForm';
-import React from 'react';
-interface CheckoutContainerProps {
-  setCartItemAmount:  React.Dispatch<React.SetStateAction<string>>;
-  cartItemAmount: string;
-}
-const CheckoutContainer = (CheckoutContainerProps : CheckoutContainerProps) : JSX.Element => {
-  const cartItems: CartItem[] = [
+import NavigationBar from "../../components/NavigationBar/NavigationBar"
+const CheckoutContainer = () : JSX.Element => {
+  const cartItems: CartItem[] = [ //Fetch from db.
     {sku: 0, displayCurrencyValue: 29.99, displayCurrencyValueType:'USD', displayCurrencyValueSymbol:'$', productImagePath:'Toy Car Blue.jpg', displayItemName: 'Toy Car Blue', quantityNumber: 4},
     {sku: 1, displayCurrencyValue: 39.99, displayCurrencyValueType:'USD', displayCurrencyValueSymbol:'$', productImagePath:'Toy Car Red.jpg', displayItemName: 'Toy Car Red', quantityNumber: 2},
     {sku: 2, displayCurrencyValue: 49.99, displayCurrencyValueType:'USD', displayCurrencyValueSymbol:'$', productImagePath:'Toy Car Green.jpg', displayItemName: 'Toy Car Green', quantityNumber: 7}];
@@ -16,15 +12,14 @@ const CheckoutContainer = (CheckoutContainerProps : CheckoutContainerProps) : JS
     for (const CartItem of cartItems) {
       ItemAmount += CartItem.quantityNumber;
     }
-    if(CheckoutContainerProps.cartItemAmount !== ItemAmount.toString())
-    {
-      CheckoutContainerProps.setCartItemAmount(ItemAmount.toString());
-    }
   return (
+    <>
+    <NavigationBar cartItemAmount={ItemAmount.toString()}/>
     <div className={classes.CheckoutContainer}>
         <OrderForm/>
         <CheckoutCart cartItems={cartItems} cartItemAmount={ItemAmount}></CheckoutCart>
     </div>
+    </>
   )
 }
 export default CheckoutContainer
--- src/pages/CheckoutPage/CheckoutPage.tsx
+++ src/pages/CheckoutPage/CheckoutPage.tsx
@@ -1,13 +1,9 @@
-import NavigationBar from "../../components/NavigationBar/NavigationBar"
 import CheckoutContainer from "../../components/CheckoutContainer/CheckoutContainer"
-import { useState } from "react"
 const CheckoutPage = () => {
-  const [cartItemAmount, setCartItemAmount] = useState('0')
   return (
     <div>
     <title>Store Checkout</title>
-    <NavigationBar cartItemAmount={cartItemAmount.toString()}/>
-    <CheckoutContainer setCartItemAmount={setCartItemAmount} cartItemAmount={cartItemAmount.toString()}/>
+    <CheckoutContainer/>
     </div>
   );
 };
