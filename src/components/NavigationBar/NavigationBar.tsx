99548affa5050a3b60311380652030a6583b89fc
Troy <TroyWarez@troysdomain.com>
2025-04-02 22:21:53 -0700

    clean up

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,13 +1,15 @@
 import classes from "./NavigationBar.module.css"
 import ImgButton from "../ImgButton/ImgButton"
+import storefrontCartIcon from "../../assets/icons/storefrontCartIcon.svg"
+import storefrontIcon from "../../assets/icons/storefrontIcon.svg"
 interface NavigationBarProps {
   cartItemAmount: number;
 }
 export const NavigationBar = ( props : NavigationBarProps)  : JSX.Element => {
   return (
     <div className={classes.navbar}>
-    <ImgButton imgPath={"storefrontIcon.png"} name={"Store"} altText={"Home"} linkPath="/"/>
-    <ImgButton className={classes.cart} imgPath={"storefrontCartIcon.png"} name={""} altText={"Cart"} linkPath="/cart"/>
+    <ImgButton imgPath={storefrontIcon} name={"Store"} altText={"Home"} linkPath="/"/>
+    <ImgButton className={classes.cart} imgPath={storefrontCartIcon} name={""} altText={"Cart"} linkPath="/cart"/>
     <p className={(props.cartItemAmount === 0) ? `${classes.cartItemAmount} ${classes.cartItemAmountHidden}` : classes.cartItemAmount}>
       {(props.cartItemAmount === 0) ? '10' : props.cartItemAmount}
       </p>
