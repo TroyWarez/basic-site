d81759102f55b2865425780e6a1b45e65b203dbc
Troy <TroyWarez@troysdomain.com>
2025-04-12 13:09:58 -0700

    added title prop

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -2,9 +2,13 @@ import classes from "./PageContainer.module.css";
 
 interface PageContainerProps {
   children: JSX.Element | JSX.Element[];
+  title: string;
 }
-const PageContainer = ({ children }: PageContainerProps) => (
-  <div className={classes.container}>{children}</div>
+const PageContainer = ({ children, title }: PageContainerProps) => (
+    <div className={classes.container}>
+    <title>{title}</title>
+    {children}
+    </div>
 );
 
 export default PageContainer;
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,7 +1,13 @@
+import PageContainer from "../../components/PageContainer/PageContainer";
+import NavigationBar from "../../components/NavigationBar/NavigationBar";
 const CartPage = () => {
   return (
-    <div>
-    </div>
+    <>
+    <NavigationBar cartItemAmount={0}/>
+    <PageContainer title="Test">
+      <p>Page</p>      
+    </PageContainer>
+    </>
   );
 };
 
