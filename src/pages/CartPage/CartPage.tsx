da0f4a9769075edefcd98554ac1e34433a1cd5cc
Troy <TroyWarez@troysdomain.com>
2025-04-12 22:58:15 -0700

    rewrite

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,15 +1,14 @@
 import classes from "./CartPage.module.css"
 import PageContainer from "../../components/PageContainer/PageContainer";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
-import TextSpan from "../../components/TextSpan/TextSpan";
 const CartPage = () => {
   return (
     <>
     <title>Shopping Cart</title>
     <NavigationBar cartItemAmount={0}/>
     <PageContainer>
-    <TextSpan className={classes.spanLarge}>Your shopping cart is empty</TextSpan>
-    <TextSpan className={classes.span}>Have any wishlist items? Sign in to view them</TextSpan>
+      <span className={classes.spanLarge}>Your shopping cart is empty</span>
+      <span className={classes.span}>Have any wishlist items? Sign in to view them</span>
     </PageContainer>
     </>
   );
