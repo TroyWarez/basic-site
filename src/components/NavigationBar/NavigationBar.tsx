160b4f64872a58e203071cd648912f08b94a7b18
Troy <TroyWarez@troysdomain.com>
2025-04-14 10:35:15 -0700

    new navbar element type and css

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -7,11 +7,11 @@ interface NavigationBarProps {
 }
 export const NavigationBar = ( { cartItemAmount } : NavigationBarProps)  : JSX.Element => {
   return (
-    <div className={classes.navbar}>
+    <header className={classes.navbar}>
     <ImgButton imgPath={storefrontIcon} name={"Store"} altText={"Home"} linkPath="/"/>
     {(cartItemAmount) ?
     <ImgButton className={classes.cart} imgPath={storefrontCartIcon} altText={"Cart"} linkPath="/cart" ImgChild={<p className={classes.badge}/>}/> : <></> }
-    </div>
+    </header>
   )
 }
 
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -5,20 +5,20 @@ const CartPage = () => {
   return (
     <>
     <title>Shopping Cart</title>
-    <NavigationBar cartItemAmount={0}/>
     <PageContainer>
+    <div className={classes.container}>
+    <NavigationBar cartItemAmount={0}/>
       <span className={`${classes.spanLarge} ${classes.span}`}>Your shopping cart is empty</span>
       <span className={classes.span}>Have any wishlist items? Sign in to view them</span>
       <a className={classes.button } href="/customer/account/login/">Sign in</a>
       <a className={`${classes.button} ${classes.buttonColored}`} href="/en/products/">Continue shopping</a>
-    </PageContainer>
-    <div className={classes.container}>
-    <div>
-    <a className={classes.footer} href="/legal/">Legal  </a>
-    <a className={`${classes.footer} ${classes.privacy}`} href="/legal/privacy/privacy-policy/">  Privacy</a>
     </div>
+    <div>
+      <a className={classes.footer} href="/legal/">Legal  </a>
+      <a className={`${classes.footer} ${classes.privacy}`} href="/legal/privacy/privacy-policy/">  Privacy</a>
     <footer className={classes.footer}>Copyright © {new Date().getFullYear()}, Company Inc. All rights reserved.</footer>
     </div>
+    </PageContainer>
     </>
   );
 };
