18aa176f184271c4bf19c61d8cc84cc5a515b4c3
Troy <TroyWarez@troysdomain.com>
2025-04-14 12:45:57 -0700

    fixed css

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -7,7 +7,9 @@ const CartPage = () => {
     <title>Shopping Cart</title>
     <PageContainer>
     <div className={classes.container}>
+    <div className={classes.navbar}>
     <NavigationBar cartItemAmount={0}/>
+    </div>
       <span className={`${classes.spanLarge} ${classes.span}`}>Your shopping cart is empty</span>
       <span className={classes.span}>Have any wishlist items? Sign in to view them</span>
       <a className={classes.button } href="/customer/account/login/">Sign in</a>
