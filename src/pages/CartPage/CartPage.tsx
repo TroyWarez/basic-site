ff1d4c2ea28845e111a2815b0731fe3f9cc0720a
Troy <TroyWarez@troysdomain.com>
2025-04-14 12:40:36 -0700

    css done for empty

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -13,12 +13,16 @@ const CartPage = () => {
       <a className={classes.button } href="/customer/account/login/">Sign in</a>
       <a className={`${classes.button} ${classes.buttonColored}`} href="/en/products/">Continue shopping</a>
     </div>
-    <div>
-      <a className={classes.footer} href="/legal/">Legal  </a>
-      <a className={`${classes.footer} ${classes.privacy}`} href="/legal/privacy/privacy-policy/">  Privacy</a>
-    <footer className={classes.footer}>Copyright © {new Date().getFullYear()}, Company Inc. All rights reserved.</footer>
-    </div>
     </PageContainer>
+    <div>
+    </div>
+    <footer className={classes.footer}>
+      
+    <div className={classes.footer}>
+    <a className={`${classes.footer} ${classes.legal}`} href="/legal/">Legal  </a>
+    <a className={`${classes.footer} ${classes.privacy}`} href="/legal/privacy/privacy-policy/">  Privacy</a>
+    </div>  
+    Copyright © {new Date().getFullYear()}, Company Inc. All rights reserved.  </footer>
     </>
   );
 };
