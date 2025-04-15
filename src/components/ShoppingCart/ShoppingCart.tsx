e6354569ca865506cb03680675344cb3e33c22f2
Troy <TroyWarez@troysdomain.com>
2025-04-15 14:51:28 -0700

    npm update added props

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import classes from "./ShoppingCart.module.css"
 import { useState } from "react";
-const ShoppingCart = () => {
+interface ShoppingCartProps {
+  className?: string;
+  SignInPagePath: string;
+  ProductPagePath: string;
+}
+const ShoppingCart = ({ className, SignInPagePath, ProductPagePath } : ShoppingCartProps) => {
   return (
-    <div className={classes.cartcontainer}>
+    <div className={`classes.cartcontainer ${className}`}>
       <span className={`${classes.spanLarge} ${classes.span}`}>Your shopping cart is empty</span>
       <span className={classes.span}>Have any wishlist items? Sign in to view them</span>
-      <Link className={classes.button } to="/customer/account/login/">Sign in</Link>
-      <Link className={`${classes.button} ${classes.buttonColored}`} to="/en/products/">Continue shopping</Link>
+      <Link className={classes.button} to={SignInPagePath}>Sign in</Link>
+      <Link className={`${classes.button} ${classes.buttonColored}`} to={ProductPagePath}>Continue shopping</Link>
     </div>
   )
 }
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -2,14 +2,13 @@ import PageContainer from "../../components/PageContainer/PageContainer";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 import StoreFooter from "../../components/StoreFooter/StoreFooter";
 import ShoppingCart from "../../components/ShoppingCart/ShoppingCart";
-
 const CartPage = () => {
   return (
     <>
     <title>Shopping Cart</title>
     <PageContainer>
       <NavigationBar cartItemAmount={0}/>
-        <ShoppingCart/>
+        <ShoppingCart SignInPagePath="/customer/account/login/" ProductPagePath="/en/products/"/>
     </PageContainer>
     <StoreFooter legal_href="/legal/" privacy_href="/legal/privacy/privacy-policy/"/>
 
