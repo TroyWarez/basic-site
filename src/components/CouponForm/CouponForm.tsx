43ae7013e91d7492e99dd8ec85e80e082c4f5fbd
Troy <TroyWarez@troysdomain.com>
2025-04-21 17:03:46 -0700

    promo code

--- src/components/CouponForm/CouponForm.tsx
+++ src/components/CouponForm/CouponForm.tsx
@@ -9,20 +9,20 @@ interface CouponFormProps {
 const CouponForm = (Props: CouponFormProps) : JSX.Element => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
+  const [isHidden, setIsHidden] = useState(true);
+  const [couponMessage, setCouponMessage] = useState('The coupon is not valid.');
   return (
     <div className={classes.CouponContainer}>
     <div className={classes.DropdownContainer} onClick={() => {
       (isCollapsed) ? setIsCollapsed(false) : setIsCollapsed(true);
     }}>
     <p className={classes.p}><b>Enter Promo Code</b> (Optional)</p>
-    <input type='button' value='>' className={`${(isCollapsed) ? classes.DropdownButtonDown : classes.DropdownButtonUp }`} onClick={() => {
-      (isCollapsed) ? setIsCollapsed(false) : setIsCollapsed(true);
-    }}
+    <input type='button' value='>' className={`${(isCollapsed) ? classes.DropdownButtonDown : classes.DropdownButtonUp }`}
     />
+    
     </div>
-
     <div className={`${(isCollapsed) ? classes.FormContainerHidden : classes.FormContainer }`}>
-    <form id={ids.CouponForm} onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
+    <form id={ids.form} onSubmit={(event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const CouponCodeInput = (event.currentTarget[0] as HTMLFormElement);
     const CouponCodeSubmitButton = (event.currentTarget[1] as HTMLFormElement);
@@ -42,20 +42,20 @@ const CouponForm = (Props: CouponFormProps) : JSX.Element => {
       couponState.then((discountPercentage) => {
         if(discountPercentage > 0) {
           Props.applyCouponDiscount(discountPercentage);
-          CouponCodeInput.style.border = '2px solid rgb(10, 160, 67)';
-          (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).hidden = false;
-          (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).textContent = `Successfully applied '${CouponCodeInput.value}'`;
-          (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).style.color = 'rgb(10, 160, 67)';
+          CouponCodeInput.style.border = '1px solid rgb(10, 160, 67)';
+          setIsHidden(false);
+          setCouponMessage(`Successfully applied '${CouponCodeInput.value}'`);
+          //setCouponMessage('rgb(10, 160, 67)');
           CouponCodeSubmitButton.hidden = true;
           CouponCodeInput.disabled = 0
           discountValid = true;
         }
         else
         {
-          CouponCodeInput.style.border = '2px solid rgb(199, 1, 1)';
-          (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).hidden = false;
-          (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).textContent = `The coupon code '${CouponCodeInput.value}' is not valid.`;
-          (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).style.color = 'rgb(199, 1, 1)';
+          CouponCodeInput.style.border = '1px solid rgb(199, 1, 1)';
+          setIsHidden(false);
+          setCouponMessage(`The coupon code '${CouponCodeInput.value}' is not valid.`);
+         // setCouponMessage('rgb(199, 1, 1)');
           CouponCodeSubmitButton.disabled = false;
           discountValid = false;
         }
@@ -72,22 +72,20 @@ const CouponForm = (Props: CouponFormProps) : JSX.Element => {
   }
   else
   {
-    CouponCodeInput.style.border = '2px solid rgb(199, 1, 1)';
-    (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).hidden = false;
-    (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).textContent = `The coupon code is too short.`;
+    CouponCodeInput.style.border = '1px solid rgb(199, 1, 1)';
+    setIsHidden(false);
+    setCouponMessage(`The coupon code '${CouponCodeInput.value}' is not valid.`);
     (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).style.color = 'rgb(199, 1, 1)';
   }
   }}>
+    <div id={ids.CouponTextInputButton}>
+
+    <div id={ids.CouponTextInputSubmitButton}>
     <FormInput className={classes.CouponInput} type="text" name="coupon" required placeholder="Promo Code" maxLength={10} id={ids.CouponCodeInput} onInput={ 
       (event: React.ChangeEvent<HTMLInputElement>) => {
       switch (event.type) {
         case "input": {
         event.target.value = event.target.value.toUpperCase().replace(/([^A-Z0-9])/g, "");
-        if((document.getElementById(ids.CouponInputError) as HTMLParagraphElement).hidden === false)
-        {
-        (document.getElementById(ids.CouponInputError) as HTMLParagraphElement).hidden = true;
-        (event.target as HTMLInputElement).style.border = '2px solid var(--main-bg-accentColor)';
-        }
         if (event.target.value.length > 0)
           {
             setIsDisabled(false);
@@ -98,8 +96,11 @@ const CouponForm = (Props: CouponFormProps) : JSX.Element => {
           }
       }
     }
-  } }>      <p id={ids.CouponInputError}>The coupon is not valid.</p></FormInput>
-      <FormInput className={classes.CouponInput} form={ids.CouponForm} disabled={isDisabled} type="submit" name="couponButton" value="Apply" required/>
+  } }/>
+        <FormInput className={classes.CouponInput} form={ids.CouponForm} disabled={isDisabled} type="submit" name="couponButton" value="Apply" required/>
+    </div>
+      <p id={ids.CouponInputError} hidden={isHidden}>{couponMessage}</p>
+      </div>
     </form>
     </div>
     </div>
