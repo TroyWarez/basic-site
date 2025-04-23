af0dc109435ad43d831558a991a73e5cbe419021
Troy <TroyWarez@troysdomain.com>
2025-04-22 17:08:59 -0700

    fixed page

--- src/pages/GuestLoginPage/GuestLoginPage.tsx
+++ src/pages/GuestLoginPage/GuestLoginPage.tsx
@@ -1,12 +1,15 @@
 import NavigationBar from "../../components/NavigationBar/NavigationBar"
 import StoreFooter from "../../components/StoreFooter/StoreFooter"
 import GuestLogin from "../../components/GuestLogin/GuestLogin"
+import PageContainer from "../../components/PageContainer/PageContainer"
 const GuestLoginPage = () => {
   return (
     <>
     <title>Guest Login</title>
+    <PageContainer>
     <NavigationBar cartItemAmount={0} />
     <GuestLogin/>
+    </PageContainer>
     <StoreFooter legal_href="/legal/" privacy_href="/legal/privacy/privacy-policy/"/>
     </>
   )
