d2e7f0cb81094196ab6de925ee802f61b8cff8aa
Troy <TroyWarez@troysdomain.com>
2025-05-07 17:34:34 -0700

    css

--- src/components/OrderDisplay/OrderDisplay.tsx
+++ src/components/OrderDisplay/OrderDisplay.tsx
@@ -10,10 +10,10 @@ const OrderDisplay = ( {className} : OrderDisplayProps) : JSX.Element => {
     return (
       <>
       <div className={`${classes.container} ${(className) ? className : ''}`}>
-          <h2>Your order was confirmed.</h2>
-          <b>{`Thank you for the order ${queryParams.get('firstName')}`}</b>
-          <b>{`An email confirmation has been sent to: ${queryParams.get('email')} .`}</b>
-          <b>{`Order Number: #${queryParams.get('OrderNumber')}`}</b>
+          <h2  className={classes.h2}>Your order was confirmed.</h2>
+          <p className={classes.p}>{`Thank you for the order ${queryParams.get('firstName')}`}</p>
+          <p className={classes.p}>{`An email confirmation has been sent to: ${queryParams.get('email')} .`}</p>
+          <p className={classes.p}>{`Order Number: #${queryParams.get('OrderNumber')}`}</p>
       </div>
       </>
     );
@@ -22,8 +22,8 @@ const OrderDisplay = ( {className} : OrderDisplayProps) : JSX.Element => {
     <>
     <div className={`${classes.container} ${(className) ? className : ''}`}>
         <h2>Failed to order your items.</h2>
-        <b>We're sorry, but the store was not able to place your order at this time.</b>
-        <b>Please try again later.</b>
+        <p className={classes.p}>We're sorry, but the store was not able to place your order at this time.</p>
+        <p className={classes.p}>Please try again later.</p>
         <Link to='/'>Go to the home page.</Link>
     </div>
     </>
