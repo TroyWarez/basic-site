2bf0e3dae7ac61060e5707e23e022a83e0da008f
Troy <TroyWarez@troysdomain.com>
2025-05-08 00:16:37 -0700

    routes

--- src/navigation/Routes.tsx
+++ src/navigation/Routes.tsx
@@ -4,12 +4,16 @@ import CartPage from "../pages/CartPage/CartPage"
 import CheckoutPage from "../pages/CheckoutPage/CheckoutPage"
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage"
 import GuestLoginPage from "../pages/GuestLoginPage/GuestLoginPage"
+import LoginPage from "../pages/LoginPage/LoginPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage";
 import OrderStatusPage from "../pages/OrderStatusPage/OrderStatusPage";
 const AppRoutes = () => (
   <Routes>
     <Route path="/checkout" element={<CheckoutPage />} />
     <Route path="/checkout/cart" element={<CartPage />} />
     <Route path="/checkout/cart/guestlogin" element={<GuestLoginPage />} />
+    <Route path="/signup" element={<SignUpPage />} />
+    <Route path="/login" element={<LoginPage />} />
     <Route path="/order-status" element={<OrderStatusPage />} />
     <Route path="/" element={<NotFoundPage />} />
     <Route path="*" element={<NotFoundPage />} />
---
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -0,0 +1,7 @@
+const SignUpPage = () : JSX.Element => {
+  return (
+    <div>SignUpPage</div>
+  )
+}
+
+export default SignUpPage
