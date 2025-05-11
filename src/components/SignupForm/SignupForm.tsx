eb902f396be674f9267c7df34235b6d3ff1da3ed
Troy <TroyWarez@troysdomain.com>
2025-05-10 22:09:07 -0700

    css

--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,4 +1,5 @@
 import checkoutClasses from '../OrderForm/OrderForm.module.css'
+import GuestClasses from '../GuestLogin/GuestLogin.module.css'
 import ShoppingCartClasses from '../ShoppingCart/ShoppingCart.module.css';
 import classes from '../SignupForm/SignupForm.module.css'
 import CartClasses from '../ShoppingCart/ShoppingCart.module.css'
@@ -15,7 +16,7 @@ const SignupForm = () : JSX.Element => {
     <FormInput type="text" pattern="[A-Za-z0-9'\.\-\s\,]+" name="username"  id="username" label="Username" error_message='Invalid Username' message='' tooShort_message='This username is too short' validation_message='Invalid Username' title="Username" required={true} minlength={2} maxLength={20}/>
     <FormInput type="password" pattern="[A-Za-z0-9'\.\-\s\,]+" name="password"  id="password" label="Password" error_message='Invalid Password' message='' tooShort_message='This password is too short' validation_message='Invalid Password' title="Password" required={true} minlength={6} maxLength={20}/>
     <FormInput type="password" pattern="[A-Za-z0-9'\.\-\s\,]+" name="confirmpassword"  id="confirmpassword" label="Re-enter Password" error_message='Invalid Password' message='Must contain numbers and symbols' tooShort_message='This password is too short' validation_message='Invalid Password' title="Password" required={true} minlength={6} maxLength={20}/>
-    <FormInput className={ShoppingCartClasses.buttonSignIn} divclassName={classes.submit} type="submit" name="submitButton" error_message='' message='' validation_message='' tooShort_message='' required={true} disabled value="Sign Up"/>
+    <FormInput className={`${ShoppingCartClasses.buttonSignIn} ${GuestClasses.button}`} divclassName={classes.submit} type="submit" name="submitButton" error_message='' message='' validation_message='' tooShort_message='' required={true} disabled value="Sign Up"/>
     <b>Already have an account? Click <Link to='/login' className={CartClasses.AltText}>here</Link> to login.</b>
     </form>
     </>
