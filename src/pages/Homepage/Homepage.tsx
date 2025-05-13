d7451b50c000f585ff30ef6458fc3a0df44b61bb
Troy <TroyWarez@troysdomain.com>
2025-05-13 15:37:03 -0700

    wip

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+import PageContainer from '../../components/PageContainer/PageContainer';
+const Homepage = () => {
+  return (<>
+      <title>Store</title>
+      <PageContainer isCentered={true}>
+      </PageContainer>
+      </> )
+}
+export default Homepage;
