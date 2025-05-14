89059305e415e67c56efccf2418644c93c5bd995
Troy <TroyWarez@troysdomain.com>
2025-05-13 23:05:00 -0700

    wip

--- src/components/ProductShowcase/ProductShowcase.tsx
+++ src/components/ProductShowcase/ProductShowcase.tsx
@@ -4,21 +4,33 @@ const ProductShowcase = (): JSX.Element  => {
   return (
   <>
   <NavigationBar/>
-    <div className={classes.container}>
-        <h1 className={classes.h1}>Products</h1>
+  <h1 className={classes.h1}>Products</h1>
+    <div className={classes.CheckboxesContainer}>
+      <h2>Filters</h2>
+    <input type='checkbox'/>
+  </div>
+    <div className={classes.ProductContainer}>
+    <div className={classes.Product}>
+        
         <div className={classes.Product}>
           <img alt='Product Image' className={classes.img} src='car.jpg'></img>
-          <p>RC Car</p>
+          <b>RC Car</b>
+          <div className={classes.ProductPriceContainer}>
+          <b className={classes.from}>Color: </b>
+          <b> Red</b>
+         </div>
           <div className={classes.ProductPriceContainer}>
             <b className={classes.from}>From: </b>
-            <b className={classes.ProductPrice}>CA$0.20</b>
+            <b className={classes.ProductPrice}>CA$45.20</b>
           </div>
-          <div className={classes.ProductPriceContainer}>
-          <p className={classes.from}>Color: </p>
-          <p> Red</p>
-        </div>
+        <div className={classes.ProductSalePriceContainer}>
+            <b className={`${classes.from} ${classes.sale}`}>From: </b>
+            <s className={`${classes.ProductPrice} ${classes.sale}`}>CA$45.20</s>
+            <b>32% off</b>
+          </div>
         </div>
     </div>
+  </div>
   </>
   )
 }
