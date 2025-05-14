179e4d81b22a2592cc8ad03650b87a12cd0a7d52
Troy <TroyWarez@troysdomain.com>
2025-05-13 21:52:18 -0700

    wip

--- src/components/ProductShowcase/ProductShowcase.tsx
+++ src/components/ProductShowcase/ProductShowcase.tsx
@@ -5,7 +5,19 @@ const ProductShowcase = (): JSX.Element  => {
   <>
   <NavigationBar/>
     <div className={classes.container}>
-        <h1>Products</h1>
+        <h1 className={classes.h1}>Products</h1>
+        <div className={classes.Product}>
+          <img alt='Product Image' className={classes.img} src='car.jpg'></img>
+          <p>RC Car</p>
+          <div className={classes.ProductPriceContainer}>
+            <b className={classes.from}>From: </b>
+            <b className={classes.ProductPrice}>CA$0.20</b>
+          </div>
+          <div className={classes.ProductPriceContainer}>
+          <p className={classes.from}>Color: </p>
+          <p> Red</p>
+        </div>
+        </div>
     </div>
   </>
   )
